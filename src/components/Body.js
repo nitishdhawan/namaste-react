856e4e49acc9749de1ffca43ff3a1dc884d1f5e5
unknown <nitish1712@gmail.com>
2024-01-21 10:32:09 -0800

    adding state hooked

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,18 +1,32 @@
 import ResturantCard from "./ResturantCard"
 import resObj from "../utils/mockData"
+import { useState } from "react"
 
 const Body = () =>{
+
+    let [listOfRestuarants,setListOfRestuarants] = useState(resObj);
+
     return(
       <div className="body">
-        <div className="search"> Search</div>
+        <div className="filter-container">
+            <button
+                className="filter-btn"
+                onClick={() => {
+                   let filteredList = listOfRestuarants.filter((res) => {
+                    return res.info.avgRating > 4.2
+                   })
+                   setListOfRestuarants(filteredList);
+                }}
+            >
+                Top Rated Restaurant
+            </button>
+        </div>
         <div className="res-container">
          {
-            resObj.map((resdata) => {
+            listOfRestuarants.map((resdata) => {
               return <ResturantCard key={resdata.info.id} resdata={resdata}/>
             })
          }
-
-
         </div>
       </div>
     )
